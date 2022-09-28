ddbf3ed53eae8210ff30fc809818971f8aa529cd
ElanYoung <991658923@qq.com>
2022-09-28 22:05:11 +0800

    feat(banner): Spring Boot 自定义 Banner

---
+++ spring-boot-banner/src/test/java/com/starimmortal/banner/BannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starimmortal.banner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
