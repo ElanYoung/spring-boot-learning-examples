fd537a56b25ce820c9c819f729b43f5c99cb4ff8
ElanYoung <991658923@qq.com>
2023-06-10 14:04:51 +0800

    feat: 消息码配置

---
+++ spring-boot-base-core/src/main/java/com/starimmortal/core/aop/ResultAspect.java
@@ -0,0 +1,37 @@
+package com.starimmortal.core.aop;
+
+import com.starimmortal.core.configuration.CodeMessageConfiguration;
+import com.starimmortal.core.vo.ResponseVO;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+/**
+ * 处理返回结果为 UnifyResponseVO 的控制器层方法
+ * message 默认为 null，在此处通过 code 设置为对应消息
+ *
+ * @author william@StarImmortal
+ * @date 2021/02/08
+ */
+@Aspect
+@Component
+public class ResultAspect {
+
+    @Autowired
+    private CodeMessageConfiguration codeMessageConfiguration;
+
+    @AfterReturning(returning = "result", pointcut = "execution(public * com.starimmortal..controller..*.*(..))")
+    public void doAfterReturning(ResponseVO<String> result) {
+        int code = result.getCode();
+        String oldMessage = result.getMessage();
+        // code-message.properties 中配置的 message
+        String newMessage = codeMessageConfiguration.getMessage(code);
+        // 如果 code-message.properties 中指定了相应的 message 并且 UnifyResponseVO 的 message 为null
+        // 则使用 newMessage 替换 oldMessage
+        if (StringUtils.hasText(newMessage) && !StringUtils.hasText(oldMessage)) {
+            result.setMessage(newMessage);
+        }
+    }
+}
---
+++ spring-boot-base-core/src/main/java/com/starimmortal/core/configuration/CodeMessageConfiguration.java
@@ -0,0 +1,28 @@
+package com.starimmortal.core.configuration;
+
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 消息码配置类
+ *
+ * @author william@StarImmortal
+ * @date 2021/02/08
+ */
+@SuppressWarnings("ConfigurationProperties")
+@ConfigurationProperties
+@PropertySource(value = "classpath:code-message.properties", encoding = "UTF-8")
+@Component
+@Getter
+public class CodeMessageConfiguration {
+    private final Map<Integer, String> codeMessage = new HashMap<>();
+
+    public String getMessage(Integer code) {
+        return codeMessage.get(code);
+    }
+}
